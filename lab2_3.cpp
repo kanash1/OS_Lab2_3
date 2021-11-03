2f0106c6ead0190f633a40710fca98575cf3581f
kanash1 <oleg3038962@gmail.com>
2021-11-03 19:57:22 +0300

    Добавьте файлы проекта.

---
+++ lab2_3.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <iostream>
+#include <Windows.h>
+
+#undef max
+
+#include "menu.h"
+
+void Read() {
+    try {
+        std::string map_name;
+
+        std::cout << "Enter map name:\n";
+        std::getline(std::cin, map_name);
+
+        HANDLE map_handle = OpenFileMappingA(FILE_MAP_ALL_ACCESS, false, map_name.data());
+        if (map_handle == nullptr) {
+            throw GetLastError();
+        }
+
+        void* map_address = MapViewOfFile(map_handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+        if (map_address == nullptr) {
+            throw GetLastError();
+        }
+
+        std::string data(static_cast<char*>(map_address));
+        std::cout << "Read data:\n" << data << '\n';
+
+    } catch (DWORD& error) {
+        std::cout << "Error " << error << " ocurred\n";
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    menu_process(Menu("READER", "Exit", std::vector<Item>({ Item("Read", Read) })));
+}
